6a95ac0f6083b43063c090ab419775d45d7fa1f5
amir hossein jahangir <a.jahangir74@gmail.com>
2023-07-25 00:29:02 +0330

    Training

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import NewTrain from "./NewTrain";
 import Hi from "./Train";
+import React from "react";
+
+export const usercontext = React.createContext()
+export const usercontexts = React.createContext()
 
 function App() {
+  let hamid = 'khaste'
+
   return (
-    <Hi />
+    <usercontext.Provider value={'mohammad'}>
+    <usercontexts.Provider value={'hamed'}>
+    <Hi hamid={hamid}/>
+    <NewTrain/>
+    </usercontexts.Provider>
+    </usercontext.Provider>
   );
 }
 
--- src/Train.js
+++ src/Train.js
@@ -1,7 +1,32 @@
-function Hi() {
+import { useEffect, useState, useContext } from "react";
+import { usercontext, usercontexts } from "./App";
+
+function Hi({hamid}) {
+  const [count, setcount] = useState(2)
+  const name = useContext(usercontext)
+  const names = useContext(usercontexts)
+
+  const handleClick = () => {
+    setcount(count + 1)
+  }
+
+  useEffect(() => {
+    console.log('amir')
+  },[])
+
   let amir = 'salam';
+  const click = (javab,e) => {
+      console.log(javab)
+      console.log(e.target)
+  }
+
   return(
-      <h1>{amir}</h1>
+      <>
+      <h1 style={{color : 'red'}}>{amir} {hamid}</h1>
+      <button onClick={(e) => click('khodafez', e)}>{name}</button>
+      <button onClick={handleClick}>{count}</button>
+      <button>{names}</button>
+      </>
   )
 }
 
