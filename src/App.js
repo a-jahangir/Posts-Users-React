811d11dcd4f72c955c6706a2048b32307f42b907
amir hossein jahangir <a.jahangir74@gmail.com>
2023-08-03 17:12:20 +0330

    New SPA

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import NewTrain from "./NewTrain";
-import Hi from "./Train";
 import React from "react";
 
-export const usercontext = React.createContext()
-export const usercontexts = React.createContext()
-
 function App() {
-  let hamid = 'khaste'
-
   return (
-    <usercontext.Provider value={'mohammad'}>
-    <usercontexts.Provider value={'hamed'}>
-    <Hi hamid={hamid}/>
-    <NewTrain/>
-    </usercontexts.Provider>
-    </usercontext.Provider>
+    <h1>salam</h1>
   );
 }
 
