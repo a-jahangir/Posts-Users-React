55107ab249ee654fd6afb109e86123b845a17e71
amir hossein jahangir <a.jahangir74@gmail.com>
2023-08-04 18:01:06 +0330

    Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <h1>salam</h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+      <Route path="/" element={<Home />}/>        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
