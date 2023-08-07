7f973ee877dcc9b0b1009b67a0a9e41b4cd31ac3
amir hossein jahangir <a.jahangir74@gmail.com>
2023-08-07 20:57:53 +0330

    Users List

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Users from "./pages/Users";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-      <Route path="/" element={<Home />}/>        
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import { NavLink } from "react-router-dom"
 
 const Header = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
                 <NavLink className="navbar-brand" to="/">Jahangir</NavLink>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <NavLink className={(navDAta) => navDAta.isActive ? "nav-link active" : "nav-link"} aria-current="page" to="/">Home</NavLink>
                         </li>
